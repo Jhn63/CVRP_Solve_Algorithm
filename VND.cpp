95a0358294d2b711e4f9b97b09f80810b2919509
Jhn63 <joaovitorteixeirabarreto150@gmail.com>
2023-10-26 16:21:26 -0300

    estrutura de vizinhaça para tercerização e ajustes guloso

---
+++ VND.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Melissa on 26/10/2023.
+//
+
+#include "VND.h"
---
+++ VND.h
@@ -0,0 +1,14 @@
+//
+// Created by Melissa on 26/10/2023.
+//
+
+#ifndef PROJETO_VND_H
+#define PROJETO_VND_H
+
+
+class VND {
+
+};
+
+
+#endif //PROJETO_VND_H
